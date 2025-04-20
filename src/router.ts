5544f83631169a9d043c9ad915743b3693f5ef2f
zorochimaru <rasim.karimli@gmail.com>
2025-04-20 18:15:07 +0400

    fix spa github-pages

--- src/router.ts
+++ src/router.ts
@@ -1,41 +1,37 @@
 import { createHashRouter } from 'react-router';
 
-import { basename, Login, routerLinks } from './core';
+import { Login, routerLinks } from './core';
 import AuthLayout from './core/layouts/AuthLayout';
 import { DashBoard, reactRouterRouter, tanstackRouter } from './modules';
 
-export const router = createHashRouter(
-  [
-    {
-      path: routerLinks.login,
-      Component: Login,
-    },
-    {
-      path: '/',
-      Component: AuthLayout,
-      children: [
-        { path: '/', Component: DashBoard },
-        { path: routerLinks.tanStackQuery, children: tanstackRouter },
-        { path: routerLinks.reactRouter, children: reactRouterRouter },
-        {
-          path: routerLinks.reactHookForm,
-          lazy: () =>
-            import('./modules/reactHookForm').then((m) => ({ Component: m.ReactHookForm })),
-        },
-        {
-          path: routerLinks.redux,
-          lazy: () => import('./modules/redux').then((m) => ({ Component: m.ReduxExample })),
-        },
-        {
-          path: routerLinks.zustand,
-          lazy: () => import('./modules/zustand').then((m) => ({ Component: m.NotesList })),
-        },
-        {
-          path: routerLinks.three,
-          lazy: () => import('./modules/three').then((m) => ({ Component: m.ThreeSphere })),
-        },
-      ],
-    },
-  ],
-  { basename: basename },
-);
+export const router = createHashRouter([
+  {
+    path: routerLinks.login,
+    Component: Login,
+  },
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      { path: '', Component: DashBoard },
+      { path: routerLinks.tanStackQuery, children: tanstackRouter },
+      { path: routerLinks.reactRouter, children: reactRouterRouter },
+      {
+        path: routerLinks.reactHookForm,
+        lazy: () => import('./modules/reactHookForm').then((m) => ({ Component: m.ReactHookForm })),
+      },
+      {
+        path: routerLinks.redux,
+        lazy: () => import('./modules/redux').then((m) => ({ Component: m.ReduxExample })),
+      },
+      {
+        path: routerLinks.zustand,
+        lazy: () => import('./modules/zustand').then((m) => ({ Component: m.NotesList })),
+      },
+      {
+        path: routerLinks.three,
+        lazy: () => import('./modules/three').then((m) => ({ Component: m.ThreeSphere })),
+      },
+    ],
+  },
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/how-to-react/",
+  base: '/how-to-react/',
 });
